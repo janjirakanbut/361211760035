6ee12008d7a381da1ba7f1234710956938372bef
janjirakanbut <Janjira.saroh@gmail.com>
2018-06-26 11:39:57 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world.");
+        System.out.println("Janjira Kanbut");
+        System.out.println("361211760035");
+        System.out.println("MIT211");
+        System.out.println("Rmutsv");
+    }  //main
+
+
+
+
+}//Class
